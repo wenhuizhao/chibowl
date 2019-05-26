2da1e210bad1b5652cd79d23688501b243cd75e9
Wayne Zhao <wzhao@prosperworks.com>
2019-05-26 13:08:52 -0700

    move controller

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "chibowl"
 set :repo_url, "git@github.com:wenhuizhao/chibowl.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{config/application.yml}
 
 set :rvm_ruby_versioni, '2.5.1'
 set :passenger_restart_with_touch, true
