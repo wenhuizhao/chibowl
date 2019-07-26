107d6040f6f7d97d7c94094b0454a2ca1d597ce3
Cindy <xinyuezhao96@gmail.com>
2019-07-25 19:37:22 -0700

    add alert for empty cart

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -13,15 +13,25 @@ function becomeChef(){
   window.location = "/chefs/become"
 }
 function viewCart(){
+  console.log('cart');
   var items = getCartItems();
   var productIds = Object.keys(items);
   var quantities = productIds.map(id=>items[id].quantity);
+  if (!items || Object.keys(items).length === 0){
+    alert('your cart is empty');
+  }
+  else
   window.location = "/orders/cart?product_ids="+productIds+"&quantities="+quantities;  
 }
 function viewCheckout(){
+  console.log('checkout');
   var items = getCartItems();
   var productIds = Object.keys(items);
   var quantities = productIds.map(id=>items[id].quantity);
+  if (!items || Object.keys(items).length === 0){
+    alert('your cart is empty, there is nothing to checkout');
+  }
+  else
   window.location = "/orders/checkout?product_ids="+productIds+"&quantities="+quantities;  
 }
 
