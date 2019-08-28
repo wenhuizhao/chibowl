5030c6d8654921175c8ed00dd75982c2d61b8e37
Cindy <xinyuezhao96@gmail.com>
2019-08-28 11:21:14 -0700

    change shipping to 0

--- app/services/yuansfer_service.rb
+++ app/services/yuansfer_service.rb
@@ -14,6 +14,7 @@ class YuansferService
     @currency = "USD"
     @timeout = 120
     @order_id = order.id.to_s
+    #@order_id = 218
     @terminal = "ONLINE"
     @goods_info = order.order_items.map do |item|
       {
@@ -86,7 +87,7 @@ class YuansferService
         req = Net::HTTP::Post.new(uri.path) 
         test_params
         param = request_params
-        #binding.pry
+        binding.pry
         req.set_form_data(param)
         res = http.request(req)
         puts "response #{res.body}"
